e9e6cf59492468e833bec5ee87bd94fadd25dac6
wcyap1027 <54693590+wcyap1027@users.noreply.github.com>
2021-06-29 21:00:28 +0800

    Create ifelse.c

---
+++ If else statements/ifelse.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 20, b = 35;
+	
+	if(a > b)
+	{
+		printf("a is greater than b");
+	}
+	else
+	{
+		printf("b is greater than a");
+	}
+	
+	return 0;
+	
+}
